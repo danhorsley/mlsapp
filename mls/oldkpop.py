0bd1cdb9b03d1660b42e242cd3edf14c797b109d
danhorsley <danielhorsley@me.com>
2023-07-18 08:32:11 +0100

    correct logic on bulk loader for next id

--- mls/oldkpop.py
+++ mls/oldkpop.py
@@ -9,7 +9,8 @@ import pytz
     
 
 #def bulk_load_data(file_path = 'mlsapp_keepajson5.csv',my_ws='boon', batch_size=300):
-def bulk_load_data(file_path = 'mlsapp_keepajson16.csv',my_ws='bestsellers', batch_size=300):
+#def bulk_load_data(file_path = 'mlsapp_keepajson16.csv',my_ws='bestsellers', batch_size=300):
+def bulk_load_data(file_path = 'mlsapp_keepajson8.csv',my_ws='66', batch_size=300):
     #with transaction.atomic():
         chunk_iterator = pd.read_csv(file_path, chunksize=batch_size)
 
@@ -18,20 +19,26 @@ def bulk_load_data(file_path = 'mlsapp_keepajson16.csv',my_ws='bestsellers', bat
 
         for chunk in chunk_iterator:
             objects=[]
+            my_next_id = Offers.objects.order_by('-id').first().id
             for _, row in chunk.iterrows():
-                counter+=1
+                counter+=1      
                 #print(row)
                 try:
                     d = datetime.strptime(row.date, '%Y-%m-%d 00:00:00')
                     my_book_id = check_or_create_static(row.book_id)
                     print(counter, d, my_book_id)
-                    obj = Offers(#id=next_id, 
+                    
+                    obj,my_bool = Offers.objects.get_or_create( 
                                 book = my_book_id, 
-                                jf = row.jf, 
-                                date = timezone.datetime(d.year, d.month, d.day, tzinfo=pytz.UTC),
-                                wholesaler = WSInfo.objects.filter(wholesaler=my_ws)[0],
-                                is_live=True)
-                    objects.append(obj)
+                                wholesaler = WSInfo.objects.filter(wholesaler=my_ws)[0],)
+                    if  my_bool:  
+                        obj.id =  my_next_id + counter    
+                        obj.jf = row.jf
+                        obj.date = timezone.datetime(d.year, d.month, d.day, tzinfo=pytz.UTC)
+                        obj.is_live=True
+                        objects.append(obj)
+                    else:
+                        pass
                 except:
                     pass
                 #counter += 1
